bbf2d73221dd71319936cdf16adef1c91e249732
AnthonyRodriguez93 <52090741+AnthonyRodriguez93@users.noreply.github.com>
2024-01-24 16:16:26 -0600

    final

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
 
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
 const db = knex({
 	client: "pg",
 	connection: {
@@ -15,12 +20,6 @@ const db = knex({
 	},
 });
 
-db.select("*")
-	.from("users")
-	.then((data) => {
-		console.log(data);
-	});
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
@@ -29,87 +28,22 @@ app.get("/", (req, res) => {
 	res.send("success");
 });
 
-app.post("/signin", (req, res) => {
-	db.select("email", "hash")
-		.from("login")
-		.where("email", "=", req.body.email)
-		.then((data) => {
-			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-			if (isValid) {
-				return db
-					.select("*")
-					.from("users")
-					.where("email", "=", req.body.email)
-					.then((user) => {
-						res.json(user[0]);
-					})
-					.catch((err) => res.status(400).json("Unable to get user"));
-			} else {
-				res.status(400).json("Wrong Credentials DX");
-			}
-		})
-		.catch((err) => res.status(400).json("Wrong Credentials DX"));
-});
-
+app.post("/signin", signin.handleSignin(db, bcrypt));
 app.post("/register", (req, res) => {
-	const { email, name, password } = req.body;
-
-	const hash = bcrypt.hashSync(password);
-	db.transaction((trx) => {
-		trx
-			.insert({
-				hash: hash,
-				email: email,
-			})
-			.into("login")
-			.returning("email")
-			.then((loginEmail) => {
-				return trx("users")
-					.returning("*")
-					.insert({
-						email: loginEmail[0].email,
-						name: name,
-						joined: new Date(),
-					})
-					.then((user) => {
-						res.json(user[0]);
-					});
-			})
-			.then(trx.commit)
-			.catch(trx.rollback);
-	}).catch((err) => res.status(400).json("Unable to register DX"));
+	register.handleRegister(req, res, db, bcrypt);
 });
-
 app.get("/profile/:id", (req, res) => {
-	const { id } = req.params;
-	db.select("*")
-		.from("users")
-		.where({ id })
-		.then((user) => {
-			if (user.length) {
-				res.json(user[0]);
-			} else {
-				res.status(400).json("Not Found DX");
-			}
-		})
-		.catch((err) => res.status(400).json("Not Found DX"));
+	profile.handleProfileGet(req, res, db);
 });
-
 app.put("/image", (req, res) => {
-	const { id } = req.body;
-	db("users")
-		.where("id", "=", id)
-		.increment("entries", 1)
-		.returning("entries")
-		.then((entries) => {
-			res.json(entries[0].entries);
-		})
-		.catch((err) => res.status(400).json("unable to get entries"));
+	image.handleImage(req, res, db);
 });
 
 app.listen(3000, () => {
 	console.log("listening on port 3000");
 });
+
+
 
 /*  
 
